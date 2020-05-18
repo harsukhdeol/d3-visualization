2dfa07e124534d1055b4619a9fac1664744bcef3
sukhd <centaurgolden@gmail.com>
2020-05-17 22:55:13 -0500

    made it orthographic

--- map.js
+++ map.js
@@ -1,15 +1,13 @@
-/* import { select, json, geoPath, geoNaturalEarth1 } from "d3";
-import { feature } from "topojson";
- */
 const svg = d3.select("svg");
 
-const projection = d3.geoNaturalEarth1();
+const projection = d3.geoOrthographic();
 const pathGenerator = d3.geoPath().projection(projection);
 
 svg
   .append("path")
   .attr("class", "sphere")
-  .attr("d", pathGenerator({ type: "Sphere" }));
+  .attr("d", pathGenerator)
+  .attr("d", d3.geoCircle);
 
 d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json").then((data) => {
   const countries = topojson.feature(data, data.objects.countries);
